0f421b2648b8cda6b7fa2f305a540c71ce2df2ab
Ramprakash438 <87698300+Ramprakash438@users.noreply.github.com>
2022-01-02 16:40:12 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,44 @@
+var resume = {
+    name: "Ram prakash",
+    address:
+      "S/o Muthu rathina pandi S, NO: 14, pallavan street, v.o.c nagar, pammal, chennai - 600075.",
+    email: "prakash438hp@gmail.com",
+    contactNo: "8610959873",
+    carrierObjective:
+      "Seeking a position that will utilize my creative thinking dedication and efficiency so that along with contributing my best to the organisation, I can continue riding on the learing curve.",
+    educationalQualification: [
+      {
+        course: "B.C.A",
+        institution: "A.M jain college, chennai",
+        university: "Madras University, chennai",
+        yearOfPassing: "2019",
+        percentageOfMark: "70.7"
+      },
+      {
+        course: "HSC",
+        institution: "A.V.B Matriculation HSS, coimbatore",
+        Board: "State Board",
+        yearOfPassing: "2016",
+        percentageOfMark: "75.6"
+      },
+      {
+        course: "SSLC",
+        institution: "St.Sebastian Matriculation HSS, chennai",
+        Board: "State Board",
+        yearOfPassing: "2014",
+        percentageOfMark: "87.6"
+      }
+    ],
+    
+   
+    personalDetails: {
+      fatherName: "Muthu rathina pandi S",
+      motherName: "Jaya Johnsi",
+      dateOfBirth: "28/01/99",
+      languagesKnown:
+        "Proficient - Tamil, English (Read, Write, Speak)"
+    }
+  };
+  
+  console.log(resume);
+  
